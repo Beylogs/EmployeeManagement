304fcd917fc26fa817752d7658410790b1e20aa2
Mang Kelogs <Mang Kelogs@MANG_KELOGS>
2025-12-04 23:52:08 +0800

    Initial COmmit - Employee Management CRUD App

---
+++ EmployeeManagement/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace EmployeeManagement.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<EmployeeManagement.Models.Employee> Employees { get; set; }
+    }
+}
---
+++ EmployeeManagement/Models/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeManagement.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required]
+        public string Position { get; set; }
+
+        [Required]
+        [Range(0, 1000000)]
+        public decimal Salary { get; set; }
+    }
+}
